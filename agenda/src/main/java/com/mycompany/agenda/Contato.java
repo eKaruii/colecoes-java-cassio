e25f9db9c0ee9e5589338197757a83416ba1c51a
eKaruii <159392951+eKaruii@users.noreply.github.com>
2025-04-09 10:51:47 -0300

    Add files via upload

---
+++ agenda/src/main/java/com/mycompany/agenda/Contato.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.agenda;
+
+/**
+ *
+ * @author alunolab08
+ */
+public class Contato {
+private String nome;
+private String numero;
+
+public Contato(String nome, String numero) {
+    this.nome = nome;
+    this.numero = numero;
+}
+
+public String getNome() {
+    return nome;
+}
+
+public String getNumero() {
+    return numero;
+}
+
+}
