1e04592ee836e88c2e3d4153aba6c5e12c8c1d9a
eKaruii <159392951+eKaruii@users.noreply.github.com>
2025-04-23 10:53:35 -0300

    Add files via upload

---
+++ Bi/src/main/java/com/mycompany/bi/Aluno.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.bi;
+
+/**
+ *
+ * @author alunolab08
+ */
+public class Aluno {
+    private String nome;
+    private Professor prof = "nenhum registrado";
+    
+    public Aluno(String nome, Professor prof){
+        this.nome = nome;
+        this.prof = prof;
+    }
+    
+    public void SetProf (Professor prof){
+        this.prof = prof;
+    }
+    
+    @Override
+    public String toString(){
+        return "Aluno{" + "nome=" + nome + ", professor=" + prof + '}';
+    }
+    
+    
+}
---
+++ Bi/src/main/java/com/mycompany/bi/Bi.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bi;
+
+/**
+ *
+ * @author alunolab08
+ */
+public class Bi {
+
+    public static void main(String[] args) {
+        Professor p1 = new Professor("cassio");
+        Aluno a1 = new Aluno("maria", p1);
+        
+        p1.imprimir;
+        
+        
+    }
+}
